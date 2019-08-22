fa28503eee70d131d3e12db3ef65e6068d028b0c
Wills Jarquin <jarquinwills@gmail.com>
2019-08-22 00:34:22 +0000

    trying fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'flixter-wills-jarquin.herokuapp.com' }  # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'https://flixter-wills-jarquin.herokuapp.com' }  # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
