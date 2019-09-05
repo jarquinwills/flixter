8d111a846fac925300c3d169f73e670d93dd62a7
Wills Jarquin <jarquinwills@gmail.com>
2019-09-05 18:53:44 +0000

    Trying fix

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module Flixter
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.web_console.whitelisted_ips = '10.0.2.2'
-    config.web_console.automount = true
     config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
