c06a15b2f11d28a4bddfccf368320d05e4a5b550
Wills Jarquin <jarquinwills@gmail.com>
2019-09-07 14:21:53 +0000

    Bugs fixed, just the comability error with mini magik is lacking on the development page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require jquery
+//= require jquery_ujs
+//= require turbolinks
 //= require popper
 //= require tether
-//= require jquery-ui
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require_tree .
+//= require jquery-ui
